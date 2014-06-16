5e80e16379285176fb49741f41e5812a691f779e
Thomas Grenfell Smith <thomathom@gmail.com>
2014-06-16 16:31:17 -0400

    Only allow admins to see the bkadmin page

--- hook_project/urls.py
+++ hook_project/urls.py
@@ -2,13 +2,16 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.admin.views.decorators import staff_member_required
+
 
 from django.contrib import admin
 
 
 urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
-    url(r"^bkadmin/$", TemplateView.as_view(template_name="badgekit_admin.html"), name="badgekit-admin"),
+    url(r"^bkadmin/$", staff_member_required(
+        TemplateView.as_view(template_name="badgekit_admin.html")), name="badgekit-admin"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
     url(r"^bk/", include("badgekit_webhooks.urls")),
